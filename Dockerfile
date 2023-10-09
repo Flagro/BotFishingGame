f73078d22a7c06b758a290be4428a85cb6d87c3c
flagro <antonpgrey@gmail.com>
2023-10-10 01:19:42 +0200

    [CODE REFACTORING] bot.py split

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-CMD ["python", "bot/bot.py"]
+CMD ["python", "bot/main.py"]
---
+++ bot/main.py
@@ -0,0 +1,23 @@
+import os
+import logging
+from dotenv import load_dotenv
+from .telegram_bot import TelegramEpicFishingBot
+
+
+def main():
+    # Load environment variables from .env file
+    load_dotenv()
+
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO
+    )
+
+    bot = TelegramEpicFishingBot(telegram_token=os.getenv('TELEGRAM_TOKEN'),
+                                 mongodb_url="mongodb://{}:{}".format(os.getenv("MONGODB_HOST"), os.getenv("MONGODB_PORT")))
+    
+    bot.run()
+
+
+if __name__ == '__main__':
+    main()
